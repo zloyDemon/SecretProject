eab6eac27d1da284625b0f1be50d9ac5d9bc0ae9
Marat <yakushev1396@gmail.com>
2022-11-19 23:28:00 +0400

    Test level and character move

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float MinLevelEdge = -33f;
+    private const float MaxLevelEdge = 385;
+    
+    [SerializeField] private Transform _target;
+    [SerializeField] private SpriteRenderer _bg;
+
+    private void Update()
+    {
+        Vector3 followVector = (Vector2)_target.position;
+
+        followVector.y = Mathf.Clamp(followVector.y, 0, 125);
+        followVector.x = Mathf.Clamp(followVector.x, MinLevelEdge, MaxLevelEdge);
+        followVector.z = transform.position.z;
+        
+        transform.position = followVector;
+
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.z = _bg.transform.position.z;
+        _bg.transform.position = cameraPosition;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 20f;
+    [SerializeField] private float _jumpForce = 2000f;
 
     private Rigidbody2D _playerRigidBody;
     private Vector2 _input;
+    private bool _isJumpPressed;
     
     private void Awake()
     {
@@ -17,6 +19,11 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         _input = new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _isJumpPressed = true;
+        }
     }
 
     private void FixedUpdate()
@@ -26,6 +33,41 @@ public class PlayerController : MonoBehaviour
 
     private void Move(Vector2 direction)
     {
+        Vector2 moveOffset = GetDirection(direction) * (_moveSpeed * 100 * Time.deltaTime);
+        float moveValue = _input.x * _moveSpeed * 100 * Time.deltaTime;
+        _playerRigidBody.velocity = new Vector2(moveValue, _playerRigidBody.velocity.y);
+        FlipX();
+        if (_isJumpPressed)
+        {
+            Jump();
+            _isJumpPressed = false;
+        }
+        
+    }
+
+    private void FlipX()
+    {
+        var xValue = _input.x;
+        Vector2 characterLocalScale = transform.localScale;
+        if (xValue > 0)
+        {
+            characterLocalScale.x = 1;
+        }
+        else if(xValue < 0)
+        {
+            characterLocalScale.x = -1;
+        }
+
+        transform.localScale = characterLocalScale;
+    }
+
+    private void Jump()
+    {
+        _playerRigidBody.velocity = new Vector2(_playerRigidBody.velocity.x, _jumpForce);
+    }
+
+    private Vector2 GetDirection(Vector2 direction)
+    {
         Vector2 raycastStart = transform.position;
         Vector2 raycastDirection = Vector2.down;
         float distance = 1.5f;
@@ -34,12 +76,6 @@ public class PlayerController : MonoBehaviour
         Vector2 normal = hit.normal;
 
         Vector2 project = direction - Vector2.Dot(direction, normal) * normal;
-        Vector2 moveOffset = project * (_moveSpeed * Time.deltaTime);
-        
-        _playerRigidBody.velocity = new Vector2(moveOffset.x, _playerRigidBody.velocity.y);
-        
-        Debug.DrawRay(raycastStart, raycastDirection * distance, Color.cyan);
-        Debug.DrawRay(raycastStart, project * distance, Color.green);
-        Debug.DrawRay(hit.point, hit.normal * distance, Color.red);
+        return project;
     }
 }
